090a7aaae5457103e989e199df96affc83768c56
hadialhamza <hamzaglory@gmail.com>
2025-05-16 18:19:22 +0600

    validation added

--- script.js
+++ script.js
@@ -13,12 +13,24 @@ function getInnerTextValue(inputId) {
   return inputAmountNumber;
 }
 
+// Button event handler
 const submitButton1 = document.getElementById("donate-submit1");
 submitButton1.addEventListener("click", function () {
   const donateAmount = getInputValue("donate-amount1");
   const donation1 = getInnerTextValue("donation1");
   const mainBalance = getInnerTextValue("main-balance");
 
+  // Validation fot NaN
+  if (isNaN(donateAmount)) {
+    alert("Please enter a valid number for donate.");
+    return;
+  }
+
+  //   Validation for Negative numbers
+  if (donateAmount < 0) {
+    alert("Please enter a positive number for donate.");
+    return;
+  }
   const totalDonation = donation1 + donateAmount;
   document.getElementById("donation1").innerText = totalDonation;
 
